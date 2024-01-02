6468ce102dcecf45d25938d6e685649f02473c90
Nguyễn Văn Quang <137971471+QuangNguyeen@users.noreply.github.com>
2024-01-02 16:34:52 +0700

    Update Cone.java

--- src/samsung/java/week3/shape/Cone.java
+++ src/samsung/java/week3/shape/Cone.java
@@ -1,6 +1,32 @@
 package samsung.java.week3.shape;
-
+import java.lang.Math;
 public class Cone extends Round {
-	
+	protected double height;
+	// Constructor
+	// getter
+	public double getHeight() {
+		return height;
+	}
+	public double getRadius() {
+		return radius;
+	}
+	// setter
+	public void setHeight(double h) {
+		this.height = h;
+	}
+	public void setRadius(double r) {
+		this.radius = r;
+	}
+	// method
+	public double getSurfaceArea() {
+        return Math.PI * radius * (radius + Math.sqrt(Math.pow(height, 2) + Math.pow(radius, 2)));
+    }
+
+	public double getTotalSurfaceArea() {
+        return Math.PI * radius * (radius + Math.sqrt(Math.pow(height, 2) + Math.pow(radius, 2))) + Math.PI * Math.pow(radius, 2);
+    }
+	public double getVolume() {
+        return (1.0 / 3.0) * Math.PI * Math.pow(radius, 2) * height;
+    }
 
 }
